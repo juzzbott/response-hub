c7b7f36061d6da331877cc455c6d1d861ef937b5
Justin McKay <juzzbott@gmail.com>
2016-02-23 17:02:30 +1100

    Started the functionality to parse the Melway map indexes from the web url.

---
+++ src/MapIndexes/ResponseHub.MapIndexParser/Parsers/IMapIndexParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enivate.ResponseHub.MapIndexParser.Parsers
+{
+	public interface IMapIndexParser
+	{
+	}
+}
