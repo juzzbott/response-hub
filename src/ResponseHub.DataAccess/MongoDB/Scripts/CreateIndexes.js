fd2e60d57f8d1adb1d619a29ae2f08943e53fe37
Justin McKay <juzzbott@gmail.com>
2016-02-25 16:41:14 +1100

    Updated index creation scripts.

--- src/ResponseHub.DataAccess/MongoDB/Scripts/CreateIndexes.js
+++ src/ResponseHub.DataAccess/MongoDB/Scripts/CreateIndexes.js
@@ -1,3 +1,10 @@
-﻿db.map_indexes.createIndex({ "GridReferences.Coordinates": "2dsphere" }, { background: true, name: "Coords_2dsphere" });
+﻿// Map indexes
+db.map_indexes.createIndex({ "GridReferences.Coordinates": "2dsphere" }, { background: true, name: "Coords_2dsphere" });
 
+// Groups
 db.groups.createIndex({ Name: "text" }, { background: true });
+db.groups.createIndex({ HeadquartersCoordinates: "2dsphere" }, { background: true, name: "HQ_Coords_2dsphere" });
+db.groups.createIndex({ "Users.UserId": 1 }, { background: true, name: "Users_UserId" })
+
+// Users
+db.users.createIndex({ FirstName: "text", Surname: "text", EmailAddress: "text" }, { background: true });
