825773fa3f26c896be78863d3d0aa83be04bba25
Justin McKay <juzzbott@gmail.com>
2016-02-27 01:04:06 +1100

    Added the unit tests project.

---
+++ src/Tests/ResponseHub.Tests.Unit/CacheTests/CacheManagerTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Enivate.ResponseHub.Caching;
+
+using Xunit;
+using System.Threading;
+
+namespace Enivate.ResponseHub.Tests.Unit.CacheTests
+{
+	public class CacheManagerTests
+	{
+
+		FakeEntity _fakeEntity;
+		string _fakeObject;
+		string _cacheKeyFakeObject = "fake_object";
+
+		public CacheManagerTests()
+		{
+
+			_fakeEntity = new FakeEntity()
+			{
+				Id = Guid.NewGuid(),
+				Name = "Fake Entity",
+				OtherData = ""
+			};
+
+			_fakeObject = "Some Object Data";
+
+			// Add the entity
+			CacheManager.AddItem(_fakeEntity);
+			CacheManager.AddItem(_cacheKeyFakeObject, _fakeObject);
+
+		}
+
+		[Fact(DisplayName = "Can Get Entity Item From Cache")]
+		[Trait("Category", "Cache Manager")]
+		public void Can_Get_Entity_Item_From_Cache()
+		{
+
+			// Get from the cache
+			FakeEntity fromCache = CacheManager.GetEntity<FakeEntity>(_fakeEntity.Id);
+
+			// Ensure it exists and there is a name
+			Assert.NotNull(fromCache);
+			Assert.True(!String.IsNullOrEmpty(fromCache.Name));
+
+		}
+
+		[Fact(DisplayName = "Can Get Object Item From Cache")]
+		[Trait("Category", "Cache Manager")]
+		public void Can_Get_Object_Item_From_Cache()
+		{
+
+			// Get from the cache
+			string fromCache = CacheManager.GetItem<string>(_cacheKeyFakeObject);
+
+			// Ensure it exists and there is a name
+			Assert.NotNull(fromCache);
+			Assert.True(!String.IsNullOrEmpty(fromCache));
+
+		}
+
+		[Fact(DisplayName = "Ensure Expired Object Not Returned From Cache")]
+		[Trait("Category", "Cache Manager")]
+		public void Ensure_Expired_Object_Not_Returned_From_Cache()
+		{
+
+			FakeEntity expiredObject = new FakeEntity()
+			{
+				Id = Guid.NewGuid(),
+				Name = "Something Expired"
+			};
+
+			CacheManager.AddItem(expiredObject, DateTime.Now.AddMilliseconds(500));
+
+			// Ensure it exists and there is a name
+			FakeEntity fromCache = CacheManager.GetEntity<FakeEntity>(expiredObject.Id);
+			Assert.NotNull(fromCache);
+			Assert.True(!String.IsNullOrEmpty(fromCache.Name));
+
+			Thread.Sleep((int)TimeSpan.FromSeconds(3).TotalMilliseconds);
+
+			// Ensure it no longer exists
+			FakeEntity fromCacheExpired = CacheManager.GetEntity<FakeEntity>(expiredObject.Id);
+			Assert.Null(fromCacheExpired);
+
+		}
+
+		[Fact(DisplayName = "Can Remove Object From Cache")]
+		[Trait("Category", "Cache Manager")]
+		public void Can_Remove_Object_From_Cache()
+		{
+
+			FakeEntity toRemoveObject = new FakeEntity()
+			{
+				Id = Guid.NewGuid(),
+				Name = "Something To Remove"
+			};
+
+			// Add to the cache
+			CacheManager.AddItem(toRemoveObject);
+
+			// Ensure it exists and there is a name
+			FakeEntity fromCache = CacheManager.GetEntity<FakeEntity>(toRemoveObject.Id);
+			Assert.NotNull(fromCache);
+			Assert.True(!String.IsNullOrEmpty(fromCache.Name));
+
+			// Remove from the cache
+			CacheManager.RemoveEntity(toRemoveObject);
+
+			// Ensure it no longer exists
+			FakeEntity fromCacheRemoved = CacheManager.GetEntity<FakeEntity>(toRemoveObject.Id);
+			Assert.Null(fromCacheRemoved);
+
+		}
+
+	}
+}
---
+++ src/Tests/ResponseHub.Tests.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ResponseHub.Tests.Unit.SolutionTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ResponseHub.Tests.Unit.SolutionTests")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c1273de7-873c-4fc6-b5b2-79d27b2b71ea")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
