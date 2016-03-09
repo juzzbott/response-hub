77d28a8685ef927e058b42e949a0646c424a21af
Justin McKay <juzzbott@gmail.com>
2016-03-09 12:28:16 +1100

    Refactored the admin search control.

--- src/ResponseHub.UI/Areas/Admin/Models/SearchViewModel.cs
+++ src/ResponseHub.UI/Areas/Admin/Models/SearchViewModel.cs
@@ -10,14 +10,17 @@ namespace Enivate.ResponseHub.UI.Areas.Admin.Models
 
 		public int Results { get; set; }
 
+		public string SearchLabel { get; set; }
+
 		public SearchViewModel()
 		{
 
 		}
 
-		public SearchViewModel(int results)
+		public SearchViewModel(int results, string searchLabel)
 		{
 			Results = results;
+			SearchLabel = searchLabel;
 		}
 
 	}
