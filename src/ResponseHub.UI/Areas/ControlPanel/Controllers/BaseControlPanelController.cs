be0247a9f3844240045b3743f9016982c2786c08
Justin McKay <juzzbott@gmail.com>
2016-05-12 16:09:26 +1000

    Started working on the Groups control panel area.

---
+++ src/ResponseHub.UI/Areas/ControlPanel/Controllers/BaseControlPanelController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Enivate.ResponseHub.UI.Controllers;
+using System.Security.Claims;
+using Enivate.ResponseHub.Model.Identity;
+
+namespace Enivate.ResponseHub.UI.Areas.ControlPanel.Controllers
+{
+    public abstract class BaseControlPanelController : BaseController
+    {
+        public IList<Guid> GetGroupIdsUserIsGroupAdmin()
+		{
+			// Get the user mappings for groups for the user
+			return new List<Guid>();
+
+		}
+    }
+}
---
+++ src/ResponseHub.UI/Areas/ControlPanel/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Enivate.ResponseHub.Common.Constants;
+using Enivate.ResponseHub.UI.Controllers;
+
+namespace Enivate.ResponseHub.UI.Areas.ControlPanel.Controllers
+{
+
+	[RouteArea("control-panel")]
+    public class HomeController : BaseControlPanelController
+    {
+
+		[Route]
+        // GET: ControlPanel/Home
+        public ActionResult Index()
+        {
+
+			// Get the group ids that the user is a group administrator of
+			IList<Guid> groupIds = GetGroupIdsUserIsGroupAdmin();
+
+			// If there is 1 group, add the context group id to the session
+			if (groupIds.Count == 1)
+			{
+				Guid groupId = groupIds.First();
+				Session[SessionConstants.GroupAdminContextGroupId] = groupId;
+				return new RedirectResult(String.Format("/control-panel/groups/{0}", groupId));
+			}
+			else
+			{
+				return View();
+			}
+
+        }
+    }
+}
