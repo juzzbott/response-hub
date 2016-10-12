0b5752c5bcd3da84980d59648269b21cd7d1a562
Justin McKay <juzzbott@gmail.com>
2016-10-12 17:49:58 +1100

    Removed the weather from the Wallboard. Created separate Weather Centre.

---
+++ src/ResponseHub.UI/Models/WeatherCentre/WeatherCentreViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Enivate.ResponseHub.Model.Warnings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Enivate.ResponseHub.UI.Models.WeatherCentre
+{
+	public class WeatherCentreViewModel
+	{
+		
+		public IDictionary<WarningSource, IWarning> Warnings { get; set; }
+
+		public RadarLoopViewModel RainRadar { get; set; }
+
+		public RadarLoopViewModel WindRadar { get; set; }
+
+		public WeatherCentreViewModel()
+		{
+			Warnings = new Dictionary<WarningSource, IWarning>();
+		}
+
+	}
+}
