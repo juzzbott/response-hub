33309c7635c46e8392c8a7357e732d15446597b5
Justin McKay <juzzbott@gmail.com>
2017-03-21 13:55:52 +1100

    Added profile view.

---
+++ src/ResponseHub.UI/Models/Profile/ProfileViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Enivate.ResponseHub.UI.Models.Profile
+{
+	public class ProfileViewModel
+	{
+
+		public string FirstName { get; set; }
+
+		public string Surname { get; set; }
+
+		public string FullName
+		{
+			get
+			{
+				return String.Format("{0} {1}", FirstName, Surname);
+			}
+		}
+
+		public DateTime DateCreated { get; set; }
+
+	}
+}
