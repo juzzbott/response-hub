a04530de15d52db69173d7f0fee0c52bc5004309
Justin McKay <juzzbott@gmail.com>
2017-03-22 19:22:38 +1100

    Started working on the "Sign On" functionality.

---
+++ src/ResponseHub.UI/Controllers/SignOnController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+using Microsoft.AspNet.Identity;
+
+using Enivate.ResponseHub.Common;
+using Enivate.ResponseHub.Model.Groups;
+using Enivate.ResponseHub.Model.Groups.Interface;
+using Enivate.ResponseHub.Model.Messages;
+using Enivate.ResponseHub.Model.Messages.Interface;
+using Enivate.ResponseHub.UI.Models.SignOn;
+
+namespace Enivate.ResponseHub.UI.Controllers
+{
+
+	[RoutePrefix("sign-on")]
+    public class SignOnController : BaseController
+	{
+
+		protected readonly ICapcodeService CapcodeService = ServiceLocator.Get<ICapcodeService>();
+		protected readonly IJobMessageService JobMessageService = ServiceLocator.Get<IJobMessageService>();
+
+		// GET: SignOn
+		[Route]
+		public async Task<ActionResult> Index()
+		{
+
+			// Get the current user id
+			Guid userId = new Guid(User.Identity.GetUserId());
+
+			// Get the capcodes for the current user
+			IList<Capcode> capcodes = await CapcodeService.GetCapcodesForUser(userId);
+
+			// Get the messages for the capcodes
+			IList<JobMessage> jobMessages = await JobMessageService.GetMostRecent(capcodes, MessageType.Job, 3);
+
+			// Create the dictionary of jobs
+			IList<Tuple<Guid, string, string>> availableOperations = new List<Tuple<Guid, string, string>>();
+			foreach(JobMessage message in jobMessages)
+			{
+				string description = message.MessageContent;
+
+				// If the length is over 100 chars, truncate it
+				if (description.Length > 100)
+				{
+					description = String.Format("{0}...", description.Substring(0, 100));
+				}
+				// Add the message to the list.
+				availableOperations.Add(new Tuple<Guid, string, string>(message.Id, description, message.JobNumber));
+			}
+
+			SignOnViewModel model = new SignOnViewModel()
+			{
+				StartDate = DateTime.Now.ToString("yyyy-MM-dd"),
+				StartTime = DateTime.Now.ToString("HH:mm"),
+				AvailableOperations = availableOperations
+			};
+
+            return View(model);
+        }
+
+		[Route]
+		[HttpPost]
+		public ActionResult Index(SignOnViewModel model)
+		{
+			return View(model);
+		}
+    }
+}
---
+++ src/ResponseHub.UI/Models/SignOn/SignOnViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Enivate.ResponseHub.UI.Models.SignOn
+{
+	public class SignOnViewModel
+	{
+
+		[Required(ErrorMessage = "Please ensure you have selected a date.")]
+		public string StartDate { get; set; }
+
+		[Required(ErrorMessage = "Please ensure a selected a time.")]
+		public string StartTime { get; set; }
+
+		[Required(ErrorMessage = "You must select a sign on type.")]
+		public int SignOnType { get; set; }
+
+		public string OperationDescription { get; set; }
+
+		public Guid? OperationJobId { get; set; }
+
+		public IList<Tuple<Guid, string, string>> AvailableOperations { get; set; }
+
+		public SignOnViewModel()
+		{
+			AvailableOperations = new List<Tuple<Guid, string, string>>();
+		}
+
+	}
+}
