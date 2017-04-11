2e3d8e47ead8496a8454675e4be60a86e1946890
Justin McKay <juzzbott@gmail.com>
2017-04-11 14:19:25 +1000

    Updated the timepicker control to be both the button and input. Set to read only for mobiles to prevent keyboard showing.

--- src/ResponseHub.UI/assets/js/modules/_core.js
+++ src/ResponseHub.UI/assets/js/modules/_core.js
@@ -120,8 +120,9 @@
 		});
 		
 		// Bind the time picker
-		$('.timepicker').datetimepicker({
+		$('.timepicker-control').datetimepicker({
 			format: 'HH:mm',
+			allowInputToggle: true,
 			icons: {
 				time: 'fa fa-fw fa-clock-o',
 				date: 'fa fa-fw fa-calendar',
@@ -136,8 +137,9 @@
 		});
 
 		// Bind the time picker
-		$('.timepicker-seconds').datetimepicker({
+		$('.timepicker-seconds-control').datetimepicker({
 			format: 'HH:mm:ss',
+			allowInputToggle: true,
 			icons: {
 				time: 'fa fa-fw fa-clock-o',
 				date: 'fa fa-fw fa-calendar',
@@ -150,6 +152,11 @@
 				close: 'fa fa-fw fa-times'
 			}
 		});
+
+		// Add read only to prevent keyboard being shown
+		if (responseHub.isMobile()) {
+			$('.timepicker input, .timepicker-seconds input, .datepicker-control input').attr('readonly', 'readonly');
+		}
 
 		// Set the graphic radioes and checkboxes
 		setGraphicRadiosCheckboxes();
