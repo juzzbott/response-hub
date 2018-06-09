ecedaf7c1f267606f89b103463921ee276245be7
Unknown <juzzbott@gmail.com>
2018-06-09 11:33:10 +1000

    Updated charts library to Charts.js

--- src/ResponseHub.UI/assets/js/modules/training.js
+++ src/ResponseHub.UI/assets/js/modules/training.js
@@ -59,15 +59,35 @@
 	function displayTrainingYearGraph() {
 		var jsonData = $('#training-overview-chart-data').val().replace(/&quot;/g, '"');
 		var chartData = JSON.parse(jsonData);
+		var ctx = document.getElementById("training-overview-chart").getContext('2d');
 
-		new Chartist.Bar('#training-overview-chart', chartData, {
-			distributeSeries: true,
-			showGridBackground: true,
-			axisY: {
-				onlyInteger: true
-			},
-			axisX: {
-				scaleMinSpace: 5
+		var chart = new Chart(ctx, {
+			type: 'bar',
+			data: chartData,
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero: true,
+							stepSize: 1,
+							userCallback: function (label, index, labels) {
+								// when the floored value is the same as the value we have a whole number
+								if (Math.floor(label) === label) {
+									return label;
+								}
+							},
+						}
+					}]
+				},
+				legend: {
+					display: false,
+				},
+				tooltips: {
+					callbacks: {
+						label: tooltipItem => `${tooltipItem.yLabel}: ${tooltipItem.xLabel}`,
+						title: () => null,
+					}
+				}
 			}
 		});
 
